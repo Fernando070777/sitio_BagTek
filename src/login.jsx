60b278b6feab9b0492716db7895127c7a1523eb2
fercha <mariafernandapinedahernandez23@gmail.com>
2026-07-21 18:46:54 -0600

    se creo un componente

---
+++ src/login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function login() {
+
+
+return(
+<section className='._login'>
+      <div className='#login'>
+        <h1>INICIO DE SESION</h1>
+       
+      </div>
+    </section>
+
+);
+}
